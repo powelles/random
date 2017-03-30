a28cf2edad33d50d57729f906e21bb4c84276408
Justin Powell <powelles@users.noreply.github.com>
2017-03-30 11:07:11 -0700

    Update random.go
    variable declaration clarity

--- random.go
+++ random.go
@@ -9,8 +9,12 @@ import (
 const chars string = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
 func newInt(i int) int {
-	r := big.NewInt(0)
-	r, err := rand.Int(rand.Reader, big.NewInt(int64(i+1)))
+	var (
+		r *big.Int
+		err error
+	)
+
+	r, err = rand.Int(rand.Reader, big.NewInt(int64(i+1)))
 	if err != nil {
 		panic(err)
 	}
